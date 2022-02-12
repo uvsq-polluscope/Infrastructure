bca23715e56938d4a721872fd448b3c1dbca4d42
Mohamed Ramdane Debiane <mohamed.debiane@ens.uvsq.fr>
2022-02-12 20:58:00 +0100

    Données brutes

--- datawarehouse/initDW.sql
+++ datawarehouse/initDW.sql
@@ -5,15 +5,3 @@ DROP DATABASE IF EXISTS test;
 SELECT 'CREATE DATABASE test'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test')\gexec
 
-
-\c test;
-CREATE TABLE test1 (
-       id int primary key
-);
-
-insert into test1 values(1);
-
-/*Peupler la base avec les données d'une campagne
-\i script.sql
-*/
-
--- pgtimeseries/initTS.sql
+++ pgtimeseries/initTS.sql
@@ -6,14 +6,3 @@ SELECT 'CREATE DATABASE test'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test')\gexec
 
 
-\c test;
-CREATE TABLE test2 (
-       id int primary key
-);
-
-insert into test2 values(1);
-
-/*Peupler la base avec les données d'une campagne
-\i script.sql
-*/
-
