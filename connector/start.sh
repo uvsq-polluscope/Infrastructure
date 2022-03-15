3b84b5c8d7c6b5047edbc1755056eb44ce0f8b06
Mohamed Ramdane Debiane <mohamed.debiane@ens.uvsq.fr>
2022-03-15 12:44:47 +0100

    Version finale

--- connector/start.sh
+++ connector/start.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8083)" != "200" ]]
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 192.168.33.124:8083)" != "200" ]]
     do sleep 5
 done
 
 echo "\n --------------Creating connectors..."
 for filename in ./*.json; do
-  curl -X POST -H "Content-Type: application/json" -d @$filename http://localhost:8083/connectors/
+  curl -X POST -H "Content-Type: application/json" -d @$filename http://192.168.33.124:8083/connectors/
 done
